a7fd9bb407e6030a22e4e970ea6e3c110ae6d205
Andreas Sahlberg <asahl@UG.KTH.SE>
2021-01-18 13:29:42 +0100

    Added files

---
+++ onsset/scenario_discovery_runs.py
@@ -0,0 +1,24 @@
+import os
+from runner import scenario
+import logging
+
+logging.basicConfig(format='%(asctime)s\t\t%(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+countries = ['ao', 'bf', 'bi', 'bj', 'bw', 'cd', 'cf', 'cg', 'ci', 'cm', 'dj', 'er', 'et', 'ga', 'gh', 'gm', 'gn', 'gq',
+             'gw', 'ke', 'km', 'lr', 'ls', 'mg', 'ml', 'mr', 'mw', 'mz', 'na', 'ne', 'ng', 'rw', 'sd', 'sl', 'sn', 'so',
+             'ss', 'st', 'sz', 'td', 'tg', 'tz', 'ug', 'za', 'zm', 'zw']
+
+countries = ['bf']
+
+for country in countries:
+    general_specs = 'C:/Users/asahl/Box Sync/Scenario_discovery/common-specs.xlsx'
+    specs_path = os.path.join('C:/Users/asahl/Box Sync/Scenario_discovery/Specs_files', "{}-1-specs.xlsx".format(country))
+    csv_path = os.path.join(
+        'C:/Users/asahl/Box Sync/EGI Energy Systems/06 Projects/2017-11 WB Electrification Platform/Paper/Input files/{}_inputs'.format(country),
+        '{}-1-country-inputs_SEPTEMBER.csv'.format(country))
+    summary_folder = os.path.join('C:/Users/asahl/Documents/Scenario_discovery', '{}_November'.format(country))
+    results_folder = os.path.join('C:/Users/asahl/Documents/Scenario_discovery', '{}'.format(country))
+    scenario(specs_path, csv_path, results_folder, summary_folder, general_specs)
+
+logging.info('Finished')
